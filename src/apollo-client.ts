b089b87f8088abe96f4deaec07b28b0eb55b4870
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2022-02-02 02:58:27 -0300

    Render user public profile

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -7,7 +7,7 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = getAccessToken();
+  const token = typeof window !== 'undefined' ? getAccessToken() : undefined;
 
   return {
     headers: {
